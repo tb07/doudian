e211e466412e49d353b63cfabbec9f74ca4b091e
tbb <>
2024-12-31 11:41:07 +0800

    获取商品

--- src/Buyin/Product/BuyinProduct.php
+++ src/Buyin/Product/BuyinProduct.php
@@ -16,7 +16,16 @@ class BuyinProduct extends BaseService
     {
         return $this->app->http->post('buyin/productsDetail', $params, false);
     }
-
+    /**
+     * 达人视角商品详情
+     * https://op.jinritemai.com/docs/api-docs/61/2866
+     * @param array $params
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     */
+    public function getBuyinKolProductsDetail(array $params)
+    {
+        return $this->app->http->post('buyin/kolProductsDetail', $params, false);
+    }
     /**
      * 商品状态查询本接口用于查询某个/批商品是否投放到联盟，以及商品的推广状态和上下架状态
      * https://op.jinritemai.com/docs/api-docs/61/1497
